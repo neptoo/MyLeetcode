f0233ce7d16a8e54702eeb0cb802df728642a120
neptoo <neptoo57@gmail.com>
2021-06-06 22:37:25 +0800

    update some notes

--- src/11SumEqualsK.js
+++ src/11SumEqualsK.js
@@ -2,6 +2,33 @@
 // 输入 nums = [1, 1, 1] k = 2
 // 输出 2，两种情况 [1, 1], [1, 1]
 
+/* const subarraySum = (nums, k) => {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      let sum = 0;
+      for (let q = i; q <= j; q++) {
+        sum += nums[q];
+      }
+      if (sum == k) count++;
+    }
+  }
+  return count;
+}; */
+
+/* const subarraySum = (nums, k) => {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      if (sum == k) count++;
+    }
+  }
+  return count;
+}; */
+
+
 var subarraySum = function (nums, k) {
   if (nums.length === 0) return 0;
   let map = { 0: 1 };
